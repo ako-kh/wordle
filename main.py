db3c331b1e57e784c62b32c5b358540227506b8f
Akaki Khorava <ako@Akakis-MacBook-Pro.local>
2025-11-28 01:41:55 +0100

    add game logic, change compare func return

--- main.py
+++ main.py
@@ -18,49 +18,84 @@ def to_dict(string):
 
 
 def compare(w1, w2):
-    string = ["_","_","_","_","_"]
+    string = ["_" for _ in range(len(w2))]
     w1_dict = to_dict(w1)
     w2_dict = to_dict(w2)
 
     if w1 == w2:
-        return w2
+        return "^" * len(w2)
     for k in w2_dict:
         try:
             if w1_dict[k] == w2_dict[k]:
                 for i in w2_dict[k]["index"]:
-                    string[i] = k
+                    string[i] = "^"
             elif w1_dict[k]["count"] >= w2_dict[k]["count"]:
                 for i in w2_dict[k]["index"]:
                     if i in w1_dict[k]["index"]:
-                        string[i] = k
+                        string[i] = "^"
                     else:
-                        string[i] = k.upper()
+                        string[i] = "!"
             else:
                 start = w2_dict[k]["count"] - w1_dict[k]["count"]
                 for i in w2_dict[k]["index"]:
                     if i in w1_dict[k]["index"]:
-                        string[i] = k
+                        string[i] = "^"
                     elif start > 0:
                         start -= 1
                     else:
-                        string[i] = k.upper()
+                        string[i] = "!"
         except KeyError:
             pass
 
     return "".join(string)
 
+def my_print(*args, end="\n"):
+    print("      ",end="")
+    for i in args:
+        print(i,end="")
+    print(end,end="")
+
 def main():
     with open("words.txt", "r") as f:
         words = f.readlines()
         word = words[randint(0, len(words))].rstrip("\n")
-        print(word)
+        # print(word)
 
+    tries = 6
+    print(
+        f"In WORDLE you got {tries} to guess the word.\n\n"
+        "Each guess must be a valid 5-letter word.\n\n"
+        "under your guess will appear next three simbols:\n"
+        "   '^' meaning the letter is in correct spot\n"
+        "   '!' meaning the letter is in the word but not in correct spot\n"
+        "   '_' meaning the letter is not in the word\n"
+    )
     while True:
-        s = compare(word, input("word: "))
+        if 0 >= tries:
+            my_print(f"The word was {word}")
+            break
+
+        input_w = input("word: ")
+        if "life+" == input_w:
+            tries += 2
+        elif 5 != len(input_w) or input_w + "\n" not in words:
+            sys.stdout.write("\x1b[1F")
+            sys.stdout.write("\x1b[0J")
+            continue
+
+        tries -= 1
+        result = compare(word, input_w)
+
         sys.stdout.write("\x1b[1F")
         sys.stdout.write("\x1b[0J")
-        print("     ", s)
         sys.stdout.flush()
+        my_print(input_w)
+        my_print(result, end="")
+        my_print(f"tries left: {tries}")
+
+        if "^^^^^" == result:
+            my_print("YOU WON")
+            break
 
 if __name__ == '__main__':
     main()
